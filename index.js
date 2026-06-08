f0aa0cca26b42ef5a55a8778430448adb35649c4
OniGir1SaN <mdtbkv.k@gmail.com>
2026-06-08 20:12:35 +0600

    add cors

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 // --- ENV ---
 const FREEDOM_MERCHANT_ID = process.env.FREEDOM_MERCHANT_ID;
 const FREEDOM_SECRET_KEY = process.env.FREEDOM_SECRET_KEY;
